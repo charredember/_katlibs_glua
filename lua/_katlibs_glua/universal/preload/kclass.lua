697d90c0ae4839a87eef44ff22049f249a2deb21
charred.ember <cember.gmod@gmail.com>
2026-02-17 18:25:53 -0600

    KClass inheritance fixes

--- lua/_katlibs_glua/universal/preload/kclass.lua
+++ lua/_katlibs_glua/universal/preload/kclass.lua
@@ -1,65 +1,72 @@
-local classes = setmetatable({},{__mode = "k"})
+local t_Merge = table.Merge
 
+local classDirectory = setmetatable({},{__mode = "k"})
 local currObj,baseClassArgs
 
-local function getObjectFactory(inheritedClass,newClass,constructor,privateTable)
+local function getObjectFactory(class,private_directory,constructor,inheritedClass)
+	local class_private = private_directory[class]
+	if inheritedClass and getmetatable(inheritedClass).__call == nil then
+		error("Inheriting from a class without a public constructor is currently unsupported!")
+	end
+
 	return function(...)
-		local newObj = setmetatable({},{__index = newClass})
-		currObj = newObj
+		local object = setmetatable({},{__index = class})
+		currObj = object
 		baseClassArgs = nil
 
-		local constructorPriv = constructor(...) or {}
+		local object_private = constructor(...) or {}
+		setmetatable(object_private,{__index = class_private})
 
 		if inheritedClass then
-			if not baseClassArgs then error("Failed to call baseclass constructor in inherited newClass!") end
-			local priv = inheritedClass(unpack(baseClassArgs))
-			table.Merge(priv,constructorPriv,true)
-			privateTable[newObj] = priv
-		else
-			privateTable[newObj] = constructorPriv
+			if not baseClassArgs then error("Failed to call KClass.CallBaseConstructor in inherited class!") end
+
+			local inheritedObject = inheritedClass(unpack(baseClassArgs))
+			for name,member in pairs(inheritedClass) do
+				if not isfunction(member) then continue end
+
+				class[name] = function(arg1,...)
+					if arg1 == object then arg1 = inheritedObject end
+					return member(arg1,...)
+				end
+			end
 		end
 
+		private_directory[object] = object_private
 		currObj = nil
-		return newObj
+		return object
 	end
 end
 
----@class KClassParams
----@field Inherit KClass?
----@field PrivateConstructor boolean?
-
 ---SHARED<br>
 ---OOP implementation<br>
----
----params:
---- - boolean? privateConstructor
---- - table? Inherit
 ---@class KClass
----@overload fun(constructor?: (fun(...): table), params?: KClassParams) : (table, fun(any: any): table?)
+---@overload fun(publicConstructor?: (fun(...): table), inheritedClass?: KClass) : (table, fun(any: any): table?)
 KClass = setmetatable({},{
-	__call = function(_,constructor,params)
-		params = params or {}
-		constructor = constructor or function(...) end
-		KError.ValidateArg(1,"constructor",KVarCondition.Function(constructor))
-		if params.Inherit then KError.ValidateArg(2,"params.Inherit",KVarCondition.Table(params.Inherit)) end
+	__call = function(_,publicConstructor,inheritedClass)
+		if publicConstructor then KError.ValidateArg(1,"constructor",KVarCondition.Function(publicConstructor)) end
+		if inheritedClass then KError.ValidateArg(2,"inheritedClass",KVarCondition.Table(inheritedClass)) end
 
-		local privateTable = setmetatable({},{_mode = "k"})
-		local function getPriv(obj) return privateTable[obj] end
+		local private_directory = {}
+		local function getPriv(obj) return private_directory[obj] end
 
-		local newClass = {}
-		classes[newClass] = true
-		privateTable[newClass] = {}
+		local class_meta = {}
+		local class = setmetatable({},class_meta)
+		private_directory[class] = {
+			GetFactory = function(privateConstructor)
+				local instantiate = getObjectFactory(class,private_directory,privateConstructor,inheritedClass)
+				return function(...)
+					return instantiate(...)
+				end
+			end,
+		}
 
-		local instantiateNewObject = getObjectFactory(params.Inherit,newClass,constructor,privateTable)
-		privateTable[newClass].__Constructor = instantiateNewObject
-
-		local classMetaTable = {}
-		classMetaTable.__index = params.Inherit
-		if not params.PrivateConstructor then
-			classMetaTable.__call = function(_,...) return instantiateNewObject(...) end
+		if publicConstructor then
+			local instantiate = getObjectFactory(class,private_directory,publicConstructor,inheritedClass)
+			class_meta.__call = function(_,...) return instantiate(...) end
 		end
-		setmetatable(newClass,classMetaTable)
-		return newClass,getPriv
+
+		classDirectory[class] = getPriv
+		return class,getPriv
 	end
 })
 
@@ -74,5 +81,6 @@ end
 ---Calls the baseclass constructor for inheritance.<br>
 ---<b><u>Can only be called inside constructors!<u/><b/>
 function KClass.CallBaseConstructor(...)
+	print("setbaseclassargs",...)
 	baseClassArgs = {...}
 end
